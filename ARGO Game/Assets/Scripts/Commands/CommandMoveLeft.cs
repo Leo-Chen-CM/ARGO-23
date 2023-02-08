68ef67de88a88c514f9b588dc211172bb416b591
Leo <c00250593@itcarlow.ie>
2023-02-08 14:38:36 +0000

    Squashed commit of the following:
    commit 950bd8e15c3a9a8a18303ad257768cc73a0635ed
    Merge: f0389f6 91b4a67
    Author: Leo <56357923+Leo-Chen-CM@users.noreply.github.com>
    Date:   Wed Feb 8 14:11:27 2023 +0000
    
        Merge pull request #2 from Leo-Chen-CM/Feat/Player_Commands
    
        Feat/player commands
    
    commit 91b4a67ff61ebd1b2a9e1a6fce1384ad00bd94a0
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Wed Feb 8 14:00:37 2023 +0000
    
        Added left/right bounds to player movement commands
    
    commit daf619884f83127446c53e210697e87a2f48889c
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Wed Feb 8 13:49:49 2023 +0000
    
        Finished mapping controls
    
    commit a5d116a85182139851ebc985e3c08f8600a9ba2d
    Merge: 9bc37e3 f0389f6
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Wed Feb 8 13:18:20 2023 +0000
    
        Merging master into my branch
    
    commit 9bc37e3a3273bb57dd1a575badc942f828ecd1bd
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Wed Feb 8 12:01:12 2023 +0000
    
        Removed Input system for the time being and player is moving
    
    commit dffa35baa1f75739725b6582dbefb12d6872d23d
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Tue Feb 7 15:41:34 2023 +0000
    
        Made a good few tweaks to the base project
    
    commit 58f2e870431215a10eee961839c972b6477d8180
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Tue Feb 7 14:25:16 2023 +0000
    
        Have jump blocked just need to implement the input handler
    
    commit bf9f7d808dc90a3baf811abc93c0bc52df55e0f7
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Tue Feb 7 13:24:50 2023 +0000
    
        Started mapping input actions
    
    commit edc9c2a07c8b30e81fb31f759c6fabbe912672eb
    Merge: 3977e66 fd13c64
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Tue Feb 7 13:03:50 2023 +0000
    
        Merge branch 'main' of https://github.com/Leo-Chen-CM/ARGO-23 into Feat/Player_Commands
    
    commit 3977e666e33186a63b1eb9939583f5bee9e8a717
    Author: JackSinnott <c00242525@itcarlow.ie>
    Date:   Tue Feb 7 13:01:00 2023 +0000
    
        Added early command classes for each player action

---
+++ ARGO Game/Assets/Scripts/Commands/CommandMoveLeft.cs
@@ -0,0 +1,26 @@
+/// <summary>
+/// Our Move command which will be shared with player and AI bots
+/// Worked on by: Jack Sinnott
+/// </summary>
+
+using UnityEngine;
+
+public class CommandMoveLeft : ICommand
+{
+    private float _leftBound = -3.5f;
+    public override void Execute(Unit t_unit, ICommand t_com)
+    {
+        Move(t_unit);
+
+        InputHandler._oldCommands.Add(t_com);
+    }
+
+    public override void Move(Unit _unit)
+    {
+        if(_unit.transform.position.x > _leftBound)
+            _unit.transform.Translate(new Vector3(-3.5f,0.0f,0.0f));
+
+        //_unit._rb.velocity -= new Vector3(-3.5f, 0.0f, 0.0f);
+    }
+}
+
