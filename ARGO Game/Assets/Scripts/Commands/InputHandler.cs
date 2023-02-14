a4fb37a330aa0d2a155ef81e8e325d4883dac143
Eoin Galavan <eoin.galavan@outlook.com>
2023-02-14 16:55:10 +0000

    AB-80 - added further components for network

--- ARGO Game/Assets/Scripts/Commands/InputHandler.cs
+++ ARGO Game/Assets/Scripts/Commands/InputHandler.cs
@@ -24,8 +24,12 @@ public class InputHandler : NetworkBehaviour
         _unit = GetComponent<Unit>();
 
     }
+    public override void OnStartServer()
+    {
+        FindObjectOfType<gameManager>().Reset();
+    }
 
-    [Client]
+        [Client]
     private void Update()
     {
         if (!isOwned) return;
