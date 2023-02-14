4a69c838601f9302b97d0878af633c52b249b6ee
JackSinnott <c00242525@itcarlow.ie>
2023-02-14 14:16:20 +0000

    AB-42 Fuzzy Implementation

--- ARGO Game/Assets/Scripts/Commands/CommandMoveRight.cs
+++ ARGO Game/Assets/Scripts/Commands/CommandMoveRight.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 public class CommandMoveRight : ICommand
 {
     private float _rightBound = 3.5f;
-    public override void Execute(Unit t_unit, ICommand t_com)
+    public void Execute(Unit t_unit, ICommand t_com)
     {
         Move(t_unit);
 
         InputHandler._oldCommands.Add(t_com);
     }
 
-    public override void Move(Unit _unit)
+    public void Move(Unit _unit)
     {
         if (_unit.transform.position.x < _rightBound)
             _unit.transform.Translate(new Vector3(3.5f, 0.0f, 0.0f));
