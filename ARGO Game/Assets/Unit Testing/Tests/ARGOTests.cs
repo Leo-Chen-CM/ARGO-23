cdae4fae3f2a9aa2c844ed70b6b85b03e7554fdd
Leo <c00250593@itcarlow.ie>
2023-02-14 16:23:17 +0000

    AB-69 Added one unit test

---
+++ ARGO Game/Assets/Unit Testing/Tests/ARGOTests.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+public class ARGOTests
+{
+    //// A Test behaves as an ordinary method
+    //[Test]
+    //public void ARGOTestsSimplePasses()
+    //{
+    //    // Use the Assert class to test conditions
+    //}
+
+
+
+    private GameObject player;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject PlayerGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Test Player"));
+
+        player = PlayerGameObject;
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(player.gameObject);
+    }
+
+
+    //[Test]
+    //public void ARGOTestsSimpleFail()
+    //{
+
+    //    //InputHandler
+
+    //    Assert.LessOrEqual(2, 1);
+    //}
+
+
+
+    [UnityTest]
+    public IEnumerator PlayerMovesLeft()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+
+        //MonoBehaviour.Instantiate(,)
+
+
+        Vector3 positionStart = player.GetComponent<Transform>().position;
+
+        CommandMoveLeft _moveLeft = new CommandMoveLeft();
+        _moveLeft.Execute(player.GetComponent<Unit>(), _moveLeft);
+
+        yield return new WaitForSeconds(1f);
+
+
+
+        Vector3 positionEnd = player.GetComponent<Transform>().position;
+
+        Assert.LessOrEqual(positionEnd.x, positionStart.x);
+    }
+}
+
+
