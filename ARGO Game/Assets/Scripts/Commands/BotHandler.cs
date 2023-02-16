876d99f156c6f8e99015725c917802742f0739f5
JackSinnott <c00242525@itcarlow.ie>
2023-02-16 12:26:52 +0000

    Bot is making it's own decisions about which lane to move to :D

--- ARGO Game/Assets/Scripts/Commands/BotHandler.cs
+++ ARGO Game/Assets/Scripts/Commands/BotHandler.cs
@@ -11,14 +11,29 @@ public class BotHandler : MonoBehaviour
         HARD = 2
     }
 
+    public enum Lane
+    {
+        LEFT_LANE = 3,
+        MIDDLE_LANE = 4,
+        RIGHT_LANE = 5
+    }
+
+
     FuzzyTest _ft;
 
     public float _test;
 
     Difficulty _diff;
+    Lane _currentLane;
+
+    bool _calculated = false;
+
     public Unit _unit;
     private ICommand _moveLeft, _moveRight, _jump, _slide; // Our buttons A, D, S, Space so we can link the button to any command at runtime
     public static List<ICommand> _oldCommands = new List<ICommand>(); // If we store commands in a list we can backtrack through commands
+
+    [SerializeField]
+    float _differential = .5f;
 
     // For our ground raycast check
     [SerializeField] private LayerMask _obstacleMask;
@@ -37,6 +52,8 @@ public class BotHandler : MonoBehaviour
         _unit = GetComponent<Unit>();
 
         _diff = Difficulty.HARD;
+
+        _currentLane = Lane.MIDDLE_LANE;
 
         _ft = GetComponent<FuzzyTest>();
     }
@@ -65,15 +82,15 @@ public class BotHandler : MonoBehaviour
 
     }
 
-    public void HandleInput(float t_temp)
+    public void HandleInput(bool t_moveLeft, bool t_moveRight)
     {
 
-        if (t_temp <= .3)
+        if (t_moveLeft)
         {
             _moveLeft.Execute(_unit, _moveLeft);
         }
 
-        if (t_temp >= .7)
+        else if (t_moveRight)
         {
             _moveRight.Execute(_unit, _moveRight);
         }
@@ -90,8 +107,61 @@ public class BotHandler : MonoBehaviour
     {
         if (Physics.Raycast(_ray, out RaycastHit _hit, t_rayDist, _obstacleMask))
         {
-            _ft.Fuzzification(t_rayDist);
+            _ft.Fuzzification(calculateMove());
         }
+    }
+
+    float calculateMove()
+    {
+        float _initial_weight = .5f;
+
+        int _percentileDifferential;
+        
+        if (!_calculated)
+        {
+
+            _percentileDifferential = Random.Range(0, 100);
+
+            Debug.Log("Percentage output: " + _percentileDifferential);
+            switch (_diff)
+            {
+                case Difficulty.EASY:
+                    if (_percentileDifferential > 0 && _percentileDifferential < 75)
+                    {
+                        _differential -= .05f;
+                    }
+                    break;
+                case Difficulty.MODERATE:
+                    if (_percentileDifferential > 0 && _percentileDifferential < 50)
+                    {
+                        _differential -= .05f;
+                    }
+                    break;
+                case Difficulty.HARD:
+                    if (_percentileDifferential > 0 && _percentileDifferential < 10)
+                    {
+                        _differential -= .05f;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            int _switchLeftRight = Random.Range(0, 1);
+            switch (_switchLeftRight)
+            {
+                case 0:
+                    _initial_weight -= _differential;
+                    break;
+                case 1:
+                    _initial_weight += _differential;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return _initial_weight;
+
     }
 
 }
