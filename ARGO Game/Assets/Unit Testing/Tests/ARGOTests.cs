7f77e1c70d7603388b2349a67ec6812dbb6466db
Leo <c00250593@itcarlow.ie>
2023-02-16 12:15:06 +0000

    AB-65 Add Unit Testing

--- ARGO Game/Assets/Unit Testing/Tests/ARGOTests.cs
+++ ARGO Game/Assets/Unit Testing/Tests/ARGOTests.cs
@@ -2,20 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
-
+using UnityEngine.UI;
 
 public class ARGOTests
 {
-    //// A Test behaves as an ordinary method
-    //[Test]
-    //public void ARGOTestsSimplePasses()
-    //{
-    //    // Use the Assert class to test conditions
-    //}
-
-
-
     private GameObject player;
     private GameObject spawner;
     private GameObject enviroment;
@@ -29,8 +21,8 @@ public class ARGOTests
         player = PlayerGameObject;
 
         spawner = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Spawner"));
-        //spawner.AddComponent<gameManager>();
-        //spawner.GetComponent<gameManager>().speed = 0.8f;
+        spawner.AddComponent<gameManager>();
+        spawner.GetComponent<gameManager>().speed = 0.8f;
         spawner.SetActive(false);
         enviroment = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Environment"));
     }
@@ -43,9 +35,27 @@ public class ARGOTests
         Object.Destroy(enviroment.gameObject);
         Object.Destroy(camObj.gameObject);
     }
-
-
     [UnityTest]
+    public IEnumerator PlayerSlides()
+    {
+
+        Vector3 scaleStart = player.GetComponent<Transform>().localScale;
+
+        CommandSlide _moveSlide = new CommandSlide();
+        _moveSlide.Execute(player.GetComponent<Unit>(), _moveSlide);
+
+        yield return new WaitForSeconds(0.5f);
+
+
+        Vector3 scaleEnd = player.GetComponent<Transform>().localScale;
+
+        Assert.Less(scaleEnd.y, scaleStart.y);
+
+        Debug.Log("Scale start: " + scaleStart.y + "\n" + "Scale End: " + scaleEnd.y);
+
+    }
+
+        [UnityTest]
     public IEnumerator PlayerJumps()
     {
         // Use the Assert class to test conditions.
@@ -90,6 +100,26 @@ public class ARGOTests
     }
 
     [UnityTest]
+    public IEnumerator PlayerMovesRight()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+
+        Vector3 positionStart = player.GetComponent<Transform>().position;
+
+        CommandMoveRight _moveRight = new CommandMoveRight();
+        _moveRight.Execute(player.GetComponent<Unit>(), _moveRight);
+
+        yield return new WaitForSeconds(1f);
+
+        Vector3 positionEnd = player.GetComponent<Transform>().position;
+
+        Assert.Greater(positionEnd.x, positionStart.x);
+
+        Debug.Log("Position start: " + positionStart + "\n" + "Position End: " + positionEnd);
+    }
+
+    [UnityTest]
     public IEnumerator Spawn()
     {
         spawner.SetActive(true);
@@ -100,6 +130,62 @@ public class ARGOTests
 
         Assert.GreaterOrEqual(gameObjects.Length,0);
     }
+
+    [UnityTest]
+    public IEnumerator CollisionTest()
+    {
+        GameObject rock = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Rock"));
+        yield return new WaitForSeconds(1f);
+        UnityEngine.Assertions.Assert.IsNull(rock);
+    }
+
+    [UnityTest]
+    public IEnumerator SceneChanger()
+    {
+        //Test Game Scene
+        SceneManager.LoadScene("MainMenu");
+        yield return new WaitForSeconds(.1f);
+
+        GameObject button = GameObject.Find("Play");
+
+        button.GetComponent<Button>().onClick.Invoke();
+
+        yield return new WaitForSeconds(1f);
+
+        Scene targetScene = SceneManager.GetSceneByBuildIndex(1);
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        Assert.AreEqual(targetScene.name, currentScene.name);
+
+        //Test credits scene
+        SceneManager.LoadScene("MainMenu");
+        yield return new WaitForSeconds(.1f);
+
+        button = GameObject.Find("Credits");
+        button.GetComponent<Button>().onClick.Invoke();
+
+
+        yield return new WaitForSeconds(1f);
+
+        targetScene = SceneManager.GetSceneByBuildIndex(2);
+        currentScene = SceneManager.GetActiveScene();
+        Assert.AreEqual(targetScene.name, currentScene.name);
+
+        //Test Settings scene
+        SceneManager.LoadScene("MainMenu");
+        yield return new WaitForSeconds(.1f);
+
+        button = GameObject.Find("Settings");
+        button.GetComponent<Button>().onClick.Invoke();
+
+
+        yield return new WaitForSeconds(1f);
+
+        targetScene = SceneManager.GetSceneByBuildIndex(3);
+        currentScene = SceneManager.GetActiveScene();
+        Assert.AreEqual(targetScene.name, currentScene.name);
+    }
+
 }
 
 
