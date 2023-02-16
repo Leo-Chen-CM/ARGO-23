d96bd331a4a10f7786b50b14e318abd21863dbab
Leo <c00250593@itcarlow.ie>
2023-02-16 10:15:19 +0000

    Test setup fleshed out more

--- ARGO Game/Assets/Unit Testing/Tests/ARGOTests.cs
+++ ARGO Game/Assets/Unit Testing/Tests/ARGOTests.cs
@@ -17,39 +17,61 @@ public class ARGOTests
 
 
     private GameObject player;
-
+    private GameObject spawner;
+    private GameObject enviroment;
+    private GameObject camObj;
     [SetUp]
     public void Setup()
     {
         GameObject PlayerGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Test Player"));
 
+        camObj = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Main Camera"));
         player = PlayerGameObject;
+
+        spawner = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Spawner"));
+        //spawner.AddComponent<gameManager>();
+        //spawner.GetComponent<gameManager>().speed = 0.8f;
+        spawner.SetActive(false);
+        enviroment = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Environment"));
     }
 
     [TearDown]
     public void Teardown()
     {
         Object.Destroy(player.gameObject);
+        Object.Destroy(spawner.gameObject);
+        Object.Destroy(enviroment.gameObject);
+        Object.Destroy(camObj.gameObject);
     }
 
 
-    [Test]
-    public void ARGOTestsSimpleFail()
+    [UnityTest]
+    public IEnumerator PlayerJumps()
     {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
 
-        //InputHandler
+        Vector3 positionStart = player.GetComponent<Transform>().position;
 
-        Assert.LessOrEqual(2, 1);
+        CommandJump _moveJump = new CommandJump();
+        _moveJump.Execute(player.GetComponent<Unit>(), _moveJump);
+
+        yield return new WaitForSeconds(0.5f);
+
+
+        Vector3 positionEnd = player.GetComponent<Transform>().position;
+
+        Assert.Greater(positionEnd.y, positionStart.y);
+
+        Debug.Log("Position start: " + positionStart.y + "\n" + "Position End: " + positionEnd.y);
     }
+
 
     [UnityTest]
     public IEnumerator PlayerMovesLeft()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
-
-        //MonoBehaviour.Instantiate(,)
-
 
         Vector3 positionStart = player.GetComponent<Transform>().position;
 
@@ -62,17 +84,21 @@ public class ARGOTests
 
         Vector3 positionEnd = player.GetComponent<Transform>().position;
 
-        Debug.Log("Position start: " + positionStart + "\n" + "Position End: " + positionEnd);
-
         Assert.Less(positionEnd.x, positionStart.x);
 
         Debug.Log("Position start: " + positionStart + "\n" + "Position End: " + positionEnd);
     }
 
     [UnityTest]
-    public IEnumerator P()
+    public IEnumerator Spawn()
     {
-        yield return new WaitForSeconds(1f);
+        spawner.SetActive(true);
+
+        yield return new WaitForSeconds(5f);
+
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Obstacle");
+
+        Assert.GreaterOrEqual(gameObjects.Length,0);
     }
 }
 
