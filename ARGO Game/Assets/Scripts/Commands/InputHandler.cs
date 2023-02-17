6fd89ad782187fc754f7ef43288996704ffea3b2
JackSinnott <c00242525@itcarlow.ie>
2023-02-17 14:08:27 +0000

    Fixing issues with player movement

--- ARGO Game/Assets/Scripts/Commands/InputHandler.cs
+++ ARGO Game/Assets/Scripts/Commands/InputHandler.cs
@@ -15,6 +15,8 @@ public class InputHandler : NetworkBehaviour
     private bool _fingerDown;
     public bool _offline;
 
+    [SerializeField] private bool onPC = true;
+
     private void Awake()
     {
         //Bind keys to commands
@@ -39,74 +41,79 @@ public class InputHandler : NetworkBehaviour
     }
     public void HandleInput()
     {
-        if (_fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == UnityEngine.TouchPhase.Began)
+        if (!onPC)
         {
-            _startPos = Input.touches[0].position;
-            _fingerDown = true;
-        }
+            if (_fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == UnityEngine.TouchPhase.Began)
+            {
+                _startPos = Input.touches[0].position;
+                _fingerDown = true;
+            }
 
-        if (_fingerDown && Input.touches[0].position.x <= _startPos.x - _pixelDistToDetect)
-        {
-            _fingerDown = false;
-            _bA.Execute(_unit, _bA);
-        }
+            if (_fingerDown && Input.touches[0].position.x <= _startPos.x - _pixelDistToDetect)
+            {
+                _fingerDown = false;
+                _bA.Execute(_unit, _bA);
+            }
 
-        if (_fingerDown && Input.touches[0].position.x >= _startPos.x + _pixelDistToDetect)
-        {
-            _fingerDown = false;
-            _bD.Execute(_unit, _bD);
-        }
+            if (_fingerDown && Input.touches[0].position.x >= _startPos.x + _pixelDistToDetect)
+            {
+                _fingerDown = false;
+                _bD.Execute(_unit, _bD);
+            }
 
-        if (_fingerDown && Input.touches[0].position.y <= _startPos.y - _pixelDistToDetect)
-        {
-            _fingerDown = false;
-            _bS.Execute(_unit, _bS);
-        }
+            if (_fingerDown && Input.touches[0].position.y <= _startPos.y - _pixelDistToDetect)
+            {
+                _fingerDown = false;
+                _bS.Execute(_unit, _bS);
+            }
 
-        if (_fingerDown && Input.touches[0].position.y >= _startPos.y + _pixelDistToDetect)
-        {
-            _fingerDown = false;
-            _bSpace.Execute(_unit, _bSpace);
-        }
-
-        // TESTING ON PC
-
-        if (_fingerDown == false && Input.GetMouseButtonDown(0))
-        {
-            _startPos = Input.mousePosition;
-            _fingerDown = true;
-        }
-
-        if (_fingerDown)
-        {
-            if (Input.mousePosition.y >= _startPos.y + _pixelDistToDetect)
+            if (_fingerDown && Input.touches[0].position.y >= _startPos.y + _pixelDistToDetect)
             {
                 _fingerDown = false;
                 _bSpace.Execute(_unit, _bSpace);
             }
-
-            else if (Input.mousePosition.y <= _startPos.y - _pixelDistToDetect)
-            {
-                _fingerDown = false;
-                _bS.Execute(_unit, _bSpace);
-            }
-
-            else if (Input.mousePosition.x >= _startPos.x + _pixelDistToDetect)
-            {
-                _fingerDown = false;
-                _bD.Execute(_unit, _bSpace);
-            }
-
-            else if (Input.mousePosition.x <= _startPos.x - _pixelDistToDetect)
-            {
-                _fingerDown = false;
-                _bA.Execute(_unit, _bSpace);
-            }
         }
-
-        if (_fingerDown && Input.GetMouseButtonUp(0))
+        // TESTING ON PC
+        if (onPC)
         {
-            _fingerDown = false;
+            if (_fingerDown == false && Input.GetMouseButtonDown(0))
+            {
+                _startPos = Input.mousePosition;
+                _fingerDown = true;
+            }
+
+            if (_fingerDown)
+            {
+                if (Input.mousePosition.y >= _startPos.y + _pixelDistToDetect)
+                {
+                    _fingerDown = false;
+                    _bSpace.Execute(_unit, _bSpace);
+                }
+
+                else if (Input.mousePosition.y <= _startPos.y - _pixelDistToDetect)
+                {
+                    _fingerDown = false;
+                    _bS.Execute(_unit, _bSpace);
+                }
+
+                else if (Input.mousePosition.x >= _startPos.x + _pixelDistToDetect)
+                {
+                    _fingerDown = false;
+                    _bD.Execute(_unit, _bSpace);
+                }
+
+                else if (Input.mousePosition.x <= _startPos.x - _pixelDistToDetect)
+                {
+                    _fingerDown = false;
+                    _bA.Execute(_unit, _bSpace);
+                }
+            }
+
+            if (_fingerDown && Input.GetMouseButtonUp(0))
+            {
+                _fingerDown = false;
+            }
+
         }
 
     }
