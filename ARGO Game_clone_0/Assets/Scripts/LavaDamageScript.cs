a350b70b701c08b1d4fd6569ece18e584d29c052
JackSinnott <c00242525@itcarlow.ie>
2023-02-28 16:52:41 +0000

    AB106 Player now has access to monobehaviour in our command scripts so we use coroutines to control sliding

---
+++ ARGO Game_clone_0/Assets/Scripts/LavaDamageScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaDamageScript : MonoBehaviour
+{
+    public GameObject gm;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gm.gameObject.GetComponent<gameManager>().reduceHealth();
+           
+        }
+    }
+}
